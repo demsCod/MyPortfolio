9c1a079a67f2e086ce31b9c563ecc8895da267df
demsCod <dembelemohamedpro430@gmail.com>
2025-06-27 18:17:30 +0200

    add my best picture for about

--- src/app/components/About.tsx
+++ src/app/components/About.tsx
@@ -3,12 +3,12 @@ export const About = () => {
     return (
         <div className="h-screen max-w-5/6 flex  px-10  max-w-5/6 flex-col ">
             <div className="h-full items-center justify-center ">
-                <img src="https://avatars.githubusercontent.com/u/151725068?v=4" alt="Mohamed Dembele" className="w-full h-1/2  mt-5 object-cover rounded-xl border-1 border-primary shadow-xl"  
+                <img src="/images/mdembele.png" alt="Mohamed Dembele" className="w-full h-1/2  mt-5 object-cover rounded-xl  border-primary "  
                         />
                 <h1 className="font-montserrat font-bold mt-5 mb-5 text-3xl">
                     ABOUT
                 </h1>
-                <hr className="border-t-2 border-primary w-full mx-auto my-4" />
+                <hr className="border-t-2 border-primary rounded-2xl w-full mx-auto my-4" />
                 <div className="flex flex-row">
                     <p className="font-poppins w-full text-lg">
                        Hey, my name is Mohamed Dembele. I’m a software developer from France, currently studying at 42 Paris. I enjoy building useful tools and love learning new tech. I’m also into hackathons, creative coding, and always looking for ways to grow through real-world projects. 
