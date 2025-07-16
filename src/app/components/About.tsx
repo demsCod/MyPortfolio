8c0b1b1cab7ee1c5dc8e6d07e6e8c3dea0be59d8
demsCod <dembelemohamedpro430@gmail.com>
2025-07-16 22:36:04 +0200

    change bg-color

--- src/app/components/About.tsx
+++ src/app/components/About.tsx
@@ -1,86 +1,104 @@
-"use client"
+"use client";
 import { Section } from "./Section";
 import { useLanguage } from "@/lib/i18n/LanguageContext";
 import Image from "next/image";
 
 export const About = () => {
-    const { t } = useLanguage();
-    
-    // Diviser la description pour pouvoir mettre en surbrillance certains mots
-    const aboutDescription = t('aboutDescription').split('\n\n');
-    
-    // Fonction pour mettre en surbrillance des mots clés
-    const highlightKeywords = (text: string) => {
-        const keywords = [
-            'software developer', 'développeur logiciel',
-            '42 Paris',
-            'useful tools', 'outils utiles',
-            'mobile app development', 'développement d\'applications mobiles',
-            'intuitive, user-focused experiences', 'expériences intuitives et centrées sur l\'utilisateur',
-            'new frameworks', 'nouveaux frameworks',
-            'languages', 'langages',
-            'development paradigms', 'paradigmes de développement',
-            'hackathons',
-            'hands-on learning', 'apprentissage pratique',
-            'challenging, real-world projects', 'projets concrets et stimulants'
-        ];
-        
-        let result = text;
-        keywords.forEach(keyword => {
-            // Éviter de remplacer des mots déjà dans un span
-            const regex = new RegExp(`(?<!<span[^>]*>)${keyword}(?![^<]*</span>)`, 'gi');
-            result = result.replace(regex, `<span class="text-primary-foreground font-medium">${keyword}</span>`);
-        });
-        
-        return result;
-    };
+  const { t } = useLanguage();
 
-    return (
-        <Section className="w-full min-h-screen flex items-center justify-center px-6 sm:px-8 md:px-12 lg:px-16 xl:px-20 py-16 md:py-20">
-            <div className=" w-5/6 flex flex-col items-center gap-8">
-                {/* Titre principal */}
-                <div className="text-center mb-6 md:mb-12">
-                    <h1 className="text-3xl md:text-4xl lg:text-5xl xl:text-6xl font-montserrat font-bold mb-4 text-primary-foreground">
-                        {t('aboutTitle')}
-                    </h1>
-                    <div className="relative">
-                        <hr className="border-t-2 border-4 border-border w-24 md:w-32 mx-auto" />
-                    </div>
-                </div>
+  // Diviser la description pour pouvoir mettre en surbrillance certains mots
+  const aboutDescription = t("aboutDescription").split("\n\n");
 
-                <div className="w-full flex flex-col lg:flex-row-reverse items-center gap-8 lg:gap-12 xl:gap-16">
-                    {/* Image Section */}
-                    <div className="w-full lg:w-1/2 flex justify-center lg:justify-end xl:justify-start order-1 lg:order-2">
-                        <div className="relative">
-                            <div className="absolute -inset-1 bg-gradient-to-r from-primary/20 to-primary/10 rounded-xl blur-md"></div>
-                            <Image
-                                loading="lazy"
-                                fetchPriority="low"
-                                priority={false}
-                                src="/images/mdembele-about.png" 
-                                alt="Mohamed Dembele" 
-                                width={450}
-                                height={450}
-                                className="relative w-64 h-64 sm:w-72 sm:h-72 md:w-80 md:h-80 lg:w-96 lg:h-96 xl:w-[450px] xl:h-[450px] 
-                                object-cover rounded-xl shadow-lg border-2 border-primary/20"  
-                            />
-                        </div>
-                    </div>
-                    
-                    {/* Text Content Section */}
-                    <div className="w-full lg:w-1/2 flex flex-col order-2 lg:order-1">
-                        <h2 className="font-montserrat font-bold text-primary-foreground text-3xl sm:text-4xl md:text-5xl mb-4">
-                            Mohamed Dembele
-                            <hr className="border-t-2 border-border rounded-2xl w-full mx-auto my-4" />
-                        </h2>
-                        <div className="font-poppins text-base sm:text-lg md:text-xl lg:text-xl xl:text-2xl/8 mb-8 leading-relaxed">
-                            {aboutDescription.map((paragraph, index) => (
-                                <p key={index} className="mb-6" dangerouslySetInnerHTML={{ __html: highlightKeywords(paragraph) }}></p>
-                            ))}
-                        </div>
-                    </div>
-                </div>
+  // Fonction pour mettre en surbrillance des mots clés
+  const highlightKeywords = (text: string) => {
+    const keywords = [
+      "software developer",
+      "développeur logiciel",
+      "42 Paris",
+      "useful tools",
+      "outils utiles",
+      "mobile app development",
+      "développement d'applications mobiles",
+      "intuitive, user-focused experiences",
+      "expériences intuitives et centrées sur l'utilisateur",
+      "new frameworks",
+      "nouveaux frameworks",
+      "languages",
+      "langages",
+      "development paradigms",
+      "paradigmes de développement",
+      "hackathons",
+      "hands-on learning",
+      "apprentissage pratique",
+      "challenging, real-world projects",
+      "projets concrets et stimulants",
+    ];
+
+    let result = text;
+    keywords.forEach((keyword) => {
+      // Éviter de remplacer des mots déjà dans un span
+      const regex = new RegExp(
+        `(?<!<span[^>]*>)${keyword}(?![^<]*</span>)`,
+        "gi"
+      );
+      result = result.replace(
+        regex,
+        `<span class=" font-medium">${keyword}</span>`
+      );
+    });
+
+    return result;
+  };
+
+  return (
+    <Section className="w-full min-h-screen flex items-center justify-center px-6 sm:px-8 md:px-12 lg:px-16 xl:px-20 py-16 md:py-20">
+      <div className=" w-5/6 flex flex-col items-center gap-8">
+        {/* Titre principal */}
+        <div className="text-center mb-6 md:mb-12">
+          <h1 className="text-3xl md:text-4xl lg:text-5xl xl:text-6xl font-montserrat font-bold mb-4 text-primary-foreground">
+            {t("aboutTitle")}
+          </h1>
+        </div>
+
+        <div className="w-full flex flex-col lg:flex-row-reverse items-center gap-8 lg:gap-12 xl:gap-16">
+          {/* Image Section */}
+          <div className="w-full lg:w-1/2 flex justify-center lg:justify-end xl:justify-start order-1 lg:order-2">
+            <div className="relative">
+              <div className="absolute -inset-1 bg-gradient-to-r from-primary/20 to-primary/10 rounded-xl blur-md"></div>
+              <Image
+                loading="lazy"
+                fetchPriority="low"
+                priority={false}
+                src="/images/mdembele-about.png"
+                alt="Mohamed Dembele"
+                width={450}
+                height={450}
+                className="relative w-64 h-64 sm:w-72 sm:h-72 md:w-80 md:h-80 lg:w-96 lg:h-96 xl:w-[450px] xl:h-[450px] 
+                                object-cover rounded-xl shadow-lg border-2 border-primary/20"
+              />
             </div>
-        </Section>
-    );
+          </div>
+
+          {/* Text Content Section */}
+          <div className="w-full lg:w-1/2 flex flex-col order-2 lg:order-1">
+            <h2 className="font-montserrat font-bold text-primary-foreground/80 text-3xl sm:text-4xl md:text-5xl mb-4">
+              Mohamed Dembele
+              <hr className="border-t-2 border-border rounded-2xl w-full mx-auto my-4" />
+            </h2>
+            <div className="font-poppins text-base sm:text-lg md:text-xl lg:text-xl xl:text-2xl/8 mb-8 leading-relaxed">
+              {aboutDescription.map((paragraph, index) => (
+                <p
+                  key={index}
+                  className="mb-6"
+                  dangerouslySetInnerHTML={{
+                    __html: highlightKeywords(paragraph),
+                  }}
+                ></p>
+              ))}
+            </div>
+          </div>
+        </div>
+      </div>
+    </Section>
+  );
 };
--- src/app/components/Hero.tsx
+++ src/app/components/Hero.tsx
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 import { Section } from "./Section";
 import { ArrowRightIcon, DownloadIcon } from "@radix-ui/react-icons";
 import Link from "next/link";
@@ -6,57 +6,59 @@ import { useLanguage } from "@/lib/i18n/LanguageContext";
 // import Image from "next/image";
 
 export const Hero = () => {
-    const { t } = useLanguage();
-    const resumeFile = "cv-mohamed-fr.pdf" ;
-    
-    return (
-        <Section className="relative flex flex-col-reverse justify-center items-center min-h-screen xl:max-h-3.5 md:flex-row md:justify-between md:items-center md:px-8 lg:px-12 xl:px-16 2xl:px-24 overflow-hidden">
-            {/* Gradient Background Effects - Inversé pour que le bas soit bg-background */}
-            <div className="absolute inset-0 bg-gradient-to-t from-background via-background/95 to-primary/15 pointer-events-none"></div>
-            
-            {/* Points lumineux repositionnés */}
-            <div className="absolute left-[-20%] top-[5%] h-[600px] w-[600px] rounded-full bg-[radial-gradient(circle_farthest-side,rgba(var(--primary),0.2),rgba(255,255,255,0))] blur-xl pointer-events-none"></div>
-            <div className="absolute right-[-20%] top-[-10%] h-[500px] w-[500px] rounded-full bg-[radial-gradient(circle_farthest-side,rgba(var(--primary),0.15),rgba(255,255,255,0))] blur-xl pointer-events-none"></div>
-            
-            {/* Transition douce vers le bas */}
-            <div className="absolute left-0 bottom-0 w-full h-40 bg-gradient-to-t from-background to-transparent pointer-events-none"></div>
-            
-            {/* Content */}
-            <div className="w-full text-center mt-8 mb-12 md:text-left md:w-1/2 md:mt-0 md:mb-0 md:pr-4 lg:pr-8 xl:pr-12 relative z-10">
-                <h3 className="text-3xl text-primary-foreground sm:text-3xl md:text-4xl lg:text-5xl xl:text-6xl 2xl:text-7xl font-montserrat font-extrabold mb-2 md:mb-3 lg:mb-4">
-                    {t('heroTitle')}
-                </h3>
-                <h2 className="text-xl sm:text-lg md:text-xl lg:text-2xl xl:text-3xl 2xl:text-4xl font-poppins font-semibold mb-4 md:mb-6 text-primary-foreground">
-                    {t('heroSubtitle')}{""}<span className="text-foreground">{t('heroOpportunities')}</span>
-                </h2>
-                <div className="flex items-center justify-center md:justify-start gap-4 mb-6">
-                    {/* Bouton Contact avec lien d'ancrage */}
-                    <Link 
-                        href="#contact" 
-                        className="inline-flex items-center bg-primary text-white px-3 py-2 md:px-4 hover:ring-2 ring-ring md:py-3 lg:px-5 lg:py-3 rounded-lg font-poppins font-semibold transition-all duration-200 hover:bg-primary/90 border-2 border-border hover:shadow-lg hover:shadow-primary/20"
-                    >
-                        {t('contactMe')}
-                        <ArrowRightIcon className="inline-block ml-2" />
-                    </Link>
-                    
-                    {/* Bouton Resume avec téléchargement */}
-                    <a 
-                        href={`/resume/${resumeFile}`}
-                        download={`Mohamed_Dembele_Resume.pdf`}
-                        className="inline-flex items-center bg-background px-3 py-2 md:px-4 md:py-3 lg:px-5 lg:py-3 rounded-lg font-poppins font-semibold transition-all duration-200 hover:ring-2 ring-ring hover:bg-card/90 border-2 border-border hover:shadow-lg"
-                    >
-                        {t('myResume')}
-                        <DownloadIcon className="inline-block ml-2" />
-                    </a>
-                </div>
-                
-                <div className="hidden font-poppins md:flex items-center gap-2 mt-8 text-sm text-foreground/70">
-                    <span className="w-10 h-0.5 bg-primary/50"></span>
-                    <span>{t('scrollDown')}</span>
-                </div>
-            </div>
-            
-            {/* <div className="w-full flex justify-center md:w-1/2 md:justify-end relative z-10">
+  const { t } = useLanguage();
+  const resumeFile = "cv-mohamed-fr.pdf";
+
+  return (
+    <Section className="relative flex flex-col-reverse justify-center items-center min-h-screen xl:max-h-3.5 md:flex-row md:justify-between md:items-center md:px-8 lg:px-12 xl:px-16 2xl:px-24 overflow-hidden">
+      {/* Gradient Background Effects - Inversé pour que le bas soit bg-background */}
+      <div className="absolute inset-0   pointer-events-none"></div>
+
+      {/* Points lumineux repositionnés */}
+      <div className="absolute left-[-20%] top-[5%] h-[600px] w-[600px] rounded-full bg-[radial-gradient(circle_farthest-side,rgba(var(--primary),0.2),rgba(255,255,255,0))] blur-xl pointer-events-none"></div>
+      <div className="absolute right-[-20%] top-[-10%] h-[500px] w-[500px] rounded-full bg-[radial-gradient(circle_farthest-side,rgba(var(--primary),0.15),rgba(255,255,255,0))] blur-xl pointer-events-none"></div>
+
+      {/* Transition douce vers le bas */}
+      <div className="absolute left-0 bottom-0 w-full h-40 bg-gradient-to-t from-background to-transparent pointer-events-none"></div>
+
+      {/* Content */}
+      <div className="w-full text-center mt-8 mb-12 md:text-left md:w-1/2 md:mt-0 md:mb-0 md:pr-4 lg:pr-8 xl:pr-12 relative z-10">
+        <h3 className="text-3xl text-primary-foreground sm:text-3xl md:text-4xl lg:text-5xl xl:text-6xl 2xl:text-7xl font-montserrat font-extrabold mb-2 md:mb-3 lg:mb-4">
+          {t("heroTitle")}
+        </h3>
+        <h2 className="text-xl sm:text-lg md:text-xl lg:text-2xl xl:text-3xl 2xl:text-4xl font-poppins font-semibold mb-4 md:mb-6 text-primary-foreground">
+          {t("heroSubtitle")}
+          {""}
+          <span className="text-foreground">{t("heroOpportunities")}</span>
+        </h2>
+        <div className="flex items-center justify-center md:justify-start gap-4 mb-6">
+          {/* Bouton Contact avec lien d'ancrage */}
+          <Link
+            href="#contact"
+            className="inline-flex items-center bg-primary text-white px-3 py-2 md:px-4 hover:ring-2 ring-ring md:py-3 lg:px-5 lg:py-3 rounded-lg font-poppins font-semibold transition-all duration-200 hover:bg-primary/90 border-2 border-border hover:shadow-lg hover:shadow-primary/20"
+          >
+            {t("contactMe")}
+            <ArrowRightIcon className="inline-block ml-2" />
+          </Link>
+
+          {/* Bouton Resume avec téléchargement */}
+          <a
+            href={`/resume/${resumeFile}`}
+            download={`Mohamed_Dembele_Resume.pdf`}
+            className="inline-flex items-center bg-background px-3 py-2 md:px-4 md:py-3 lg:px-5 lg:py-3 rounded-lg font-poppins font-semibold transition-all duration-200 hover:ring-2 ring-ring hover:bg-card/90 border-2 border-border hover:shadow-lg"
+          >
+            {t("myResume")}
+            <DownloadIcon className="inline-block ml-2" />
+          </a>
+        </div>
+
+        <div className="hidden font-poppins md:flex items-center gap-2 mt-8 text-sm text-foreground/70">
+          <span className="w-10 h-0.5 bg-primary/50"></span>
+          <span>{t("scrollDown")}</span>
+        </div>
+      </div>
+
+      {/* <div className="w-full flex justify-center md:w-1/2 md:justify-end relative z-10">
                 <div className="relative">
                     <div className="absolute -inset-1 bg-gradient-to-r from-primary/30 to-primary/10 rounded-xl blur-md"></div>
                     <Image
@@ -73,6 +75,6 @@ export const Hero = () => {
                     />
                 </div>
             </div> */}
-        </Section>
-    );
-}
+    </Section>
+  );
+};
